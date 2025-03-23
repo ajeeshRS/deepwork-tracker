1b3370b61f9be9e052dc19f790ffd730d8101fcb
ajeeshRS <ajeeshrs79@gmail.com>
2025-03-23 22:25:52 +0530

    fix mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import TimerComponent from "@/components/timer";
-import { Timer } from "@/components/timer-component";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="w-screen h-screen flex items-center justify-center md:p-0 p-5">
       <TimerComponent />
     </div>
   );
